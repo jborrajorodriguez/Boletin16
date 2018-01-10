315ae1505831365fddbe6221fb5c30fb6ed1b64c
Juan <Juan@Juan-PC>
2018-01-10 18:51:19 +0100

    Clase boletin 16 con nuevo metodo de notas

--- src/boletin16/Boletin16.java
+++ src/boletin16/Boletin16.java
@@ -9,12 +9,22 @@ import javax.swing.JOptionPane;
 public class Boletin16 {
     
     public static void main(String[] args) {
-        Persoal a1=new Persoal(652879654,"a1@gmail.com");
-        Academia a=new Academia(9,"Juan",a1);
-        JOptionPane.showMessageDialog(null,a.toString());
-        Persoal a2=new Persoal(652879654,"a2@gmail.com");
-        Academia a3=new Academia(9,"Arturo",a2);
-        JOptionPane.showMessageDialog(null,a3.toString());
+//        Persoal a1=new Persoal(652879654,"a1@gmail.com");
+//        Academia a=new Academia(9,"Juan",a1);
+//        JOptionPane.showMessageDialog(null,a.toString());
+//        Persoal a2=new Persoal(652879654,"a2@gmail.com");
+//        Academia a3=new Academia(9,"Arturo",a2);
+//        JOptionPane.showMessageDialog(null,a3.toString());
+        Persoal alumno=new Persoal();
+        Academia alumno1=new Academia();
+        alumno1.setNome(JOptionPane.showInputDialog("Introduce nombre del alumno"));
+        alumno.setCorreo(JOptionPane.showInputDialog("Introduce el mail del alumno"));
+        alumno.setTelefono(Integer.parseInt(JOptionPane.showInputDialog("Introducir telefono")));
+        alumno1.añadirNota();
+        alumno1.setAlum(alumno);
+        JOptionPane.showMessageDialog(null,alumno1.toString());
+        
+        
         
         
         
