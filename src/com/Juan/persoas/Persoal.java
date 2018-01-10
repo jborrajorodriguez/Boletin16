97ac6de0703a7a4ffa71933e460177f59c193cea
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-01-10 09:59:57 +0100

    Start project

---
+++ src/com/Juan/persoas/Persoal.java
@@ -0,0 +1,54 @@
+package com.Juan.persoas;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Persoal {
+    private int telefono;
+    private String correo;
+    /**
+     * Constructor por defecto.
+     */
+    public Persoal() {
+    }
+    /**
+     * Constructor con parametros
+     * @param telefono numero de telefono de la persona.
+     * @param correo nombre del correo electronico de la persona.
+     */
+    public Persoal(int telefono,String correo) {
+        this.telefono=telefono;
+        this.correo=correo;
+    }
+    /**
+     * Método getTelefono,devolve o valor de telefono.
+     * @return telefono
+     */
+    public int getTelefono() {
+        return telefono;
+    }
+    /**
+     * Método setTelefono,lle asigna un valor a telefono
+     * @param telefono 
+     */
+    public void setTelefono(int telefono) {
+        this.telefono=telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo=correo;
+    }
+
+    @Override
+    public String toString() {
+        return ("Telefono : "+telefono+"\nCorreo : "+correo);
+    }
+    
+    
+    
+    
+
+}
